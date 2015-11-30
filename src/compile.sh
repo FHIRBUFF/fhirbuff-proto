34700fa1c4df5388b2c095623801c217340fc160
Cryptovote <cryptovote@yandex.com>
2015-11-29 20:49:18 -0600

    added compile for other languages, C#, Java, Objective C.

--- src/compile.sh
+++ src/compile.sh
@@ -1,8 +1,35 @@
-
-rm  bin/*
+mkdir -p bin
+rm -rf bin/*
+#Compile for C++
 protoc -I=. --cpp_out=bin/. ./primitives.proto
 protoc -I=. --cpp_out=bin/. ./location.proto
 protoc -I=. --cpp_out=bin/. ./person.proto
 protoc -I=. --cpp_out=bin/. ./substance.proto
 protoc -I=. --cpp_out=bin/. ./practitioner.proto
 protoc -I=. --cpp_out=bin/. ./healthcareservice.proto
+
+
+#Compile for C#
+protoc -I=. --csharp_out=bin/. ./primitives.proto
+protoc -I=. --csharp_out=bin/. ./location.proto
+protoc -I=. --csharp_out=bin/. ./person.proto
+protoc -I=. --csharp_out=bin/. ./substance.proto
+protoc -I=. --csharp_out=bin/. ./practitioner.proto
+protoc -I=. --csharp_out=bin/. ./healthcareservice.proto
+
+
+#Compile for Objective C
+protoc -I=. --objc_out=bin/. ./primitives.proto
+protoc -I=. --objc_out=bin/. ./location.proto
+protoc -I=. --objc_out=bin/. ./person.proto
+protoc -I=. --objc_out=bin/. ./substance.proto
+protoc -I=. --objc_out=bin/. ./practitioner.proto
+protoc -I=. --objc_out=bin/. ./healthcareservice.proto
+
+#Compile for Java
+protoc -I=. --java_out=bin/. ./primitives.proto
+protoc -I=. --java_out=bin/. ./location.proto
+protoc -I=. --java_out=bin/. ./person.proto
+protoc -I=. --java_out=bin/. ./substance.proto
+protoc -I=. --java_out=bin/. ./practitioner.proto
+protoc -I=. --java_out=bin/. ./healthcareservice.proto
